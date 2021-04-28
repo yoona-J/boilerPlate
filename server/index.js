1627e7458f5ee052d7e1e0db1e548c29affbe0cb
yoona <yoonalove77@naver.com>
2021-04-29 02:45:16 +0900

    BoilerPlate

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const port = 4000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-const {auth} = require('./middleware/auth');
+const {auth} = require('../middleware/auth');
 const {User} = require("./models/User");
 
 
@@ -98,6 +98,18 @@ app.get('/api/users/auth', auth, (req, res) => {
     role: req.user.role,
     image: req.user.image
   })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({_id: req.user._id}, 
+    {token : ""}
+    , (err, user) => {
+      if (err) return res.json ({ success: false, err});
+      return res.status(200).send ({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {
